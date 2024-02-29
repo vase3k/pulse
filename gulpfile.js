910096fdc70de948734413c0933db190167a22d4
Vasiliy VV <vase3k@gmail.com>
2024-02-29 12:36:17 +0300

    50 header block and advantages

--- gulpfile.js
+++ gulpfile.js
@@ -20,10 +20,7 @@ gulp.task('styles', function () {
     return gulp.src("src/sass/**/*.+(scss|sass)")
         .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
         .pipe(rename({ suffix: '.min', prefix: '' }))
-        .pipe(autoprefixer({
-            overrideBrowserslist: ['last 2 versions'],
-            cascade: false
-        }))
+        .pipe(autoprefixer())
         .pipe(cleanCSS({ compatibility: "ie8" }))
         .pipe(gulp.dest("src/css"))
         .pipe(browserSync.stream());
