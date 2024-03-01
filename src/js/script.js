cf06f314e34830fd2e56d8c14503a7c7a9a74be5
Vasiliy VV <vase3k@gmail.com>
2024-03-01 23:52:11 +0300

    57 tiny slider - need adaptive

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,11 @@
-$(document).ready(function () {
-    $('.carousel__inner').slick({
-        infinite: true,
-        speed: 1000,
-        prevArrow: '<button type="button" class="slick-prev"><img src="./icons/sliderLeftArrow.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="./icons/sliderRightArrow.svg"></button>',
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                    dots: true,
-                    arrows: false
-                }
-            }
-        ]
-    });
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    nav: false
 });
+
+document.querySelector('.prev').addEventListener('click', () => slider.goTo('prev'));
+document.querySelector('.next').addEventListener('click', () => slider.goTo('next'));
