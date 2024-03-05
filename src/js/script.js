a2a5da605d18b02718a331a925001af47a50db30
Vasiliy VV <vase3k@gmail.com>
2024-03-05 18:01:49 +0300

    scroll

--- src/js/script.js
+++ src/js/script.js
@@ -159,6 +159,22 @@ $(document).ready(function () {
             $('form').trigger('reset');
         });
         return false;
-    })
+    });
+
+    //smooth scroll and pageup
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href^='#']").click(function () {
+        const _href = $(this).attr('href');
+        $('html, body').animate({ scrollTop: $(_href).offset().top + "px" });
+        return false;
+    });
+
 });
 
